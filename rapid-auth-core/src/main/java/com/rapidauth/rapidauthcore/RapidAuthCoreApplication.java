63285feb780e6f6396903f8c0b0491a271959096
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2026-02-09 19:56:26 +0100

    Initialize the core module

---
+++ rapid-auth-core/src/main/java/com/rapidauth/rapidauthcore/RapidAuthCoreApplication.java
@@ -0,0 +1,13 @@
+package com.rapidauth.rapidauthcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidAuthCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RapidAuthCoreApplication.class, args);
+    }
+
+}
